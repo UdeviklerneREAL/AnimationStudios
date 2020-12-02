592d91702461621e421a5c47586d1db348f7413f
Your Name <you@example.com>
2020-12-02 16:58:28 +0100

    add

--- src/main/java/com/udeGames/animationStudios/saving/SLSImplementation.java
+++ src/main/java/com/udeGames/animationStudios/saving/SLSImplementation.java
@@ -10,14 +10,14 @@ public class SLSImplementation {
     public static void saveImGUILayout (HashMap<String, ImBoolean> imBooleanHashMap) {
         SLSMain slsMain = new SLSMain(System.getProperty("user.dir"), SLSMain.FileOrDir.CREATEDIR);
         for (String keys : imBooleanHashMap.keySet())  {
-            slsMain.encodeBoolean(imBooleanHashMap.get(keys).get(), keys, "UILayout.sls");
+            slsMain.encodeBoolean(imBooleanHashMap.get(keys).get(), keys, "UILayout.saveLoadSystem");
         }
     }
 
     public static ImBoolean loadImGuiLayout(String var) {
         SLSMain slsMain = new SLSMain(System.getProperty("user.dir"), SLSMain.FileOrDir.CREATEDIR);
         try {
-            return new ImBoolean(slsMain.decodeBoolean(var, "UILayout.sls"));
+            return new ImBoolean(slsMain.decodeBoolean(var, "UILayout.saveLoadSystem"));
         } catch (RuntimeException e) {
             return null;
         }
